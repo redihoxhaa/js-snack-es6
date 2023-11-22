ea6c67bdc22210afedc8b65e4d2fec756b28dab0
redihoxhaa <redi.hoxha1996@gmail.com>
2023-11-22 14:54:00 +0100

    feat: scaffolding

---
+++ Snack 1/js/myscript.js
@@ -0,0 +1,6 @@
+"use strict";
+
+/* Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore utilizzando destructuring e template literal */
---
+++ Snack 3/js/myscript.js
@@ -0,0 +1,6 @@
+"use strict";
+
+/* Bonus Snack 3
+Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+Usiamo i nuovi metodi degli array foreach o filter. */
